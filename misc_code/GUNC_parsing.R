924c33205a8b1a3bec6a37dbabf35dfa23d88816
EisenRa <raph.eisenhofer@gmail.com>
2024-05-31 13:56:20 +0200

    -fixes to computational estimates -GUNC figure/stats -DAMR min and SD

--- misc_code/GUNC_parsing.R
+++ misc_code/GUNC_parsing.R
@@ -6,33 +6,32 @@
 library(tidyverse)
 
 df <- read_delim("data/GUNC.progenomes_2.1.maxCSS_level.tsv") %>%
-  mutate(strategy = case_when(str_detect(genome, "ALL_dreped") ~ "all_dereplicated",
-                              str_detect(genome, "coassembly_cage_treat") ~ "coassembly_cage_treat",
-                              str_detect(genome, "coassembly_treat") ~ "coassembly_treat",
-                              str_detect(genome, "coassembly_long") ~ "coassembly_long",
-                              str_detect(genome, "cobinning_cage_treat") ~ "cobinning_cage_treat",
-                              str_detect(genome, "cobinning_treat") ~ "cobinning_treat",
-                              str_detect(genome, "cobinning_long") ~ "cobinning_long",
-                              str_detect(genome, "vamb_cage_treat") ~ "vamb_cage_treat",
-                              str_detect(genome, "vamb_treat") ~ "vamb_treat",
-                              str_detect(genome, "vamb_long") ~ "vamb_long",
-                              str_detect(genome, "individual") ~ "individual",
-                              ),
-         )
+  filter(!str_detect(genome, "ALL_dreped")) %>%
+  mutate(overall_strategy = case_when(
+    str_detect(genome, "individual") ~ "single_coverage",
+    str_detect(genome, "cobinning_long") ~ "multicoverage_animal",
+    str_detect(genome, "cobinning_treat") ~ "multicoverage_timepoint_all",
+    str_detect(genome, "cobinning_cage_treat") ~ "multicoverage_timepoint_cage",
+    str_detect(genome, "coassembly_long") ~ "coassembly_animal",
+    str_detect(genome, "coassembly_treat") ~ "coassembly_timepoint_all",
+    str_detect(genome, "coassembly_cage_treat") ~ "coassembly_timepoint_cage",
+    str_detect(genome, "vamb_long") ~ "multisplit_animal",
+    str_detect(genome, "vamb_treat") ~ "multisplit_timepoint_all",
+    str_detect(genome, "vamb_cage_treat") ~ "multisplit_timepoint_cage",
+    TRUE ~ genome))
 
 
 #Plotting
 #How many bins pass GUNC threshold per strategy?
 df %>% 
-  filter(strategy != "cobinning_treat") %>%
-  summarise(n = n(), .by = c(strategy, pass.GUNC)) %>%
-  ggplot(aes(x = strategy, y = n, fill = pass.GUNC)) +
+  summarise(n = n(), .by = c(overall_strategy, pass.GUNC)) %>%
+  ggplot(aes(x = overall_strategy, y = n, fill = pass.GUNC)) +
   geom_bar(stat = "identity", position = position_stack(reverse = FALSE)) +
   theme_classic() +
-  theme(axis.text.x = element_text(angle = 90, size = 14, hjust = 1),
+  theme(axis.text.x = element_text(angle = 45, size = 14, hjust = 1),
         axis.text.y = element_text(size = 14),
         axis.title = element_text(size = 16, face = "bold")) +
-  labs(y = "n bins")
+  labs(y = "Number of bins", x = "Overall strategy")
   
 
 #N contigs per strategy?
@@ -51,4 +50,4 @@ df %>%
 
 #Save summary for another figure
 
-write_delim(df %>% select(genome, n_contigs, pass.GUNC), "data/gunc_summary.tsv")
+write_delim(df %>% select(genome, n_contigs, pass.GUNC, overall_strategy), "data/gunc_summary.tsv")
